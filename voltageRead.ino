4b8fa1bf84d0b4bad033aa062ed34bb16e3dc52d
GerryZhang7 <Gerryzhang123@gmail.com>
2019-03-04 11:56:52 -0500

    Added code to read input voltage of photodiodes

---
+++ voltageRead.ino
@@ -0,0 +1,18 @@
+#inlcude <Servo.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // This line will read the input voltage of the photodiode. 
+  int analogIn = analogRead(A0);
+
+  // This line converts the analog input of the arduino to voltage 
+  float voltage = ((5.0 * analogIn)/1023.0);
+
+  // Prints voltage input to terminal
+  Serial.println(voltage);
+}
